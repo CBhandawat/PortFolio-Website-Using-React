7d335d4df8f5c714ed08edd42f3c7a22108a5f97
Chetanyaa Bhandawat <chetanyabhandawat@gmail.com>
2024-10-09 20:28:14 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,7 @@ export default function About() {
             id="about"
             className="px-10 w-full flex flex-col lg:flex-row py-20 
                        align-center bg-emerald-200 max-w-5xl mx-auto">
-            <div className="flex-1">
-                <img src="https://cbhandawat.github.io/PortFolio-Website-Using-React/chetanyaabhandawat.png"
-                    alt="About"
-                     />
-            </div>
+           
             <div className="flex-1 flex flex-col justify-center
                             items-center gap-5 px-6">
                 <div>
